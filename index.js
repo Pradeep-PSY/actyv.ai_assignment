c259f499e9d085f2f86b56a4385185bf6a7d4b7e
Danny12244 <pradeep.py725@gmail.com>
2022-11-19 18:31:36 +0530

    dsa part

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-const a = 0
+// done 1 question
+
+let arr = [1, 2, 3, -1];
+
+let target = 2;
+let sum = 0;
+for (let i = 0; i < arr.length - 1; i++) {
+  sum = arr[i] + arr[i + 1];
+
+  if (sum == target) {
+    console.log(arr[i], arr[i + 1]);
+  }
+}
+
+console.log(sum);
+
+
+//done 3 question
+let sent = "This is a test sentence";
+sent = sent.split(" ");
+let rever = "";
+for (let i = sent.length - 1; i >= 0; i--) {
+  rever += sent[i] + " ";
+}
+
+console.log(rever);
+
+
+
+// done 4 question
+let sent_arr = ["This", "is", "a", "test", "array", "a", "test"];
+let obj = {},
+  sent_rev_arr = [];
+for (let j = 0; j < sent_arr.length; j++) {
+  if (obj[sent_arr[j]] == undefined) {
+    obj[sent_arr[j]] = 1;
+    sent_rev_arr.push(sent_arr[j]);
+  }
+}
+
+console.log(sent_rev_arr);
+
+
+// 2 question
+let num = 234;
+ num = 234 % 10;
+console.log(num)
